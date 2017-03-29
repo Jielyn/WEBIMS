2233f15d4f7e54a80390ab4c03e15aabdefb61bb
caolei <clei118@foxmail.com>
2017-03-29 10:08:57 +0800

    服务搭建

--- models/baseModels.js
+++ models/baseModels.js
@@ -1,3 +1,18 @@
 /**
  * Created by Lin on 2017/3/28.
  */
+//模拟dao层
+
+var mysql = require("mysql");
+
+exports.login = function (db,res,data) {
+    var loginSql = "SELECT COUNT(1) FROM USER";
+    db.query(
+        loginSql,
+            function (err,rows) {
+                if (err) throw err;
+                res.end("没有查找到");
+            }
+    );
+};
+
--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,21 @@
 /**
  * Created by Lin on 2017/3/28.
  */
+var baseMange = require("../models/baseModels");
+
+
+
+module.exports = function (app,db) {
+
+    app.get('/login',function (req,res) {
+        //var data = req.body;
+        //baseMange.login(db,res,data);
+
+    });
+
+    /*app.post('',function (req,res) {
+        
+    });*/
+
+
+};
